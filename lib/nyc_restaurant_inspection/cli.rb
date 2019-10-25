42555a4b9ce8ee4d5dbcd0df4fa75f0c79ed3165
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-25 22:51:32 +0000

    new

--- lib/nyc_restaurant_inspection/cli.rb
+++ lib/nyc_restaurant_inspection/cli.rb
@@ -11,42 +11,38 @@ class NYCRestaurantInspection::CLI
   end
 
 
-  def options
-    @options = NYCRestaurantInspection::Restaurant.insp_results
-    # @options.each.with_index(1) do |option, i|
-    #   puts "#{i}. #{deal}"
+  def list_restaurants
+    @restaurants = NYCRestaurantInspection::Restaurant.insp_results
+    @restaurants.each.with_index(1) do |restaurant, i|
+      puts "#{i}. #{restaurant.name} - #{restaurant.boro} - #{restaurant.zip_code}"
+    end
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "Please select from the list or type exit:"
       puts <<-DOC.gsub /^\s*/, ''
+        Please select from the list or type exit:
         1. Search by name
         2. Search by zip code
         3. Search by BORO [Manhattan/Brooklyn/Bronx/Queens/Staten Island]
       DOC
       input = gets.strip.downcase
 
-      # if input.to_i > 0
-      #   puts @options[input.to_i-1]
-      # elsif input == "list"
-      #
-      #   options
-      case input
-        when "1"
-          puts "1"
-        when "2"
-          puts "2"
-        when "exit"
-           leave_page
-        else
-          puts "Meh, not sure what you want..."
-        end
+      if input.to_i > 0
+        puts @restaurants[input.to_i-1]
+      elsif input == "list"
+        list_restaurants
+      # elsif "exit"
+      #   leave_page
+      else
+        puts "Meh, not sure what you want..."
       end
+    end
   end
 
-  def leave_page
+  def leave_pagey
+
     puts "Eat, drink, be healthy! Goodbye!"
   end
 
