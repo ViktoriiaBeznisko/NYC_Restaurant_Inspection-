6240a1cb4a9b2b18ecaaaaf685b78d6399077a9c
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-25 05:01:48 +0000

    1st update

---
+++ lib/NYC_Restaurant_Inspection/cli.rb
@@ -0,0 +1,9 @@
+class NYCRestaurantInspection::CLI
+
+  def call
+    puts "Welcome to New York City Restaurant Inspection tracker!"
+    puts "Do you really want to know Health Inspection result for your favorite restaurant?"
+    #puts "Eat, drink, be healthy! NYC"
+  end
+
+end
