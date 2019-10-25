257b3862aff31016ffe9f30f4be8dd38d8856131
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-25 06:54:24 +0000

    3rd update

--- lib/nyc_restaurant_inspection/cli.rb
+++ lib/nyc_restaurant_inspection/cli.rb
@@ -2,8 +2,26 @@ class NYCRestaurantInspection::CLI
 
   def call
     puts "Welcome to New York City Restaurant Inspection tracker!"
-    puts "Do you really want to know Health Inspection result for your favorite restaurant?"
-    #puts "Eat, drink, be healthy! NYC"
+    
+    puts "Do you really want to know Health Inspection result for your favorite restaurant?[y/n]"
+      if gets.strip == "y"
+        find_restaurant
+      else 
+        leave_page  
+      end      
   end
 
+  def find_restaurant
+    puts "Please select from the list:"
+    #need to make it real
+    puts <<-DOC
+      1. hola hola
+      2. lovee
+    DOC
+  end
+
+  def leave_page
+    puts "Eat, drink, be healthy! Goodbye!"
+  end
+  
 end
