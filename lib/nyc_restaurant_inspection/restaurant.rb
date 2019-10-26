84dbed3d1b65a27a534ab16e0283e89f89359375
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-26 06:10:42 +0000

    new

--- lib/nyc_restaurant_inspection/restaurant.rb
+++ lib/nyc_restaurant_inspection/restaurant.rb
@@ -15,16 +15,22 @@ class NYCRestaurantInspection::Restaurant
 
   def self.scrape
     doc = Nokogiri::HTML(open("https://data.cityofnewyork.us/Health/DOHMH-New-York-City-Restaurant-Inspection-Results/rs6k-p7g6"))
+    table = doc.at('card.card.ng-isolate-scope')
+
+    table.search('tr').each do |tr|
+    cells = tr.search('th, td')
+
+    cells.each do |cell|
+    text = cell.text.strip
+
+    puts CSV.generate_line(cells)
+
     #doc.css(".cell.context")
     binding.pry
   end
 end
 
-#name = doc.search('table-row.div.cell.text').text
-
-# tables = doc.search('card.card.ng-isolate-scope').text
-
-
+#name = doc.search('.cell.text').text.strip
 #NYCRestaurantInspection::Restaurant.scrape
 
 # rest_1 = self.new
