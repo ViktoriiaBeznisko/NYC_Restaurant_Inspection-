307fa7c0d8b01f07e3b002473d008f0cc9c427dc
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-26 19:01:27 +0000

    123

--- lib/nyc_restaurant_inspection/restaurant.rb
+++ lib/nyc_restaurant_inspection/restaurant.rb
@@ -25,12 +25,15 @@ class NYCRestaurantInspection::Restaurant
   end
 
   def self.scrape
-    doc = Nokogiri::HTML(open("https://data.cityofnewyork.us/Health/DOHMH-New-York-City-Restaurant-Inspection-Results/rs6k-p7g6"))
-    # https://data.cityofnewyork.us/Health/DOHMH-New-York-City-Restaurant-Inspection-Results/43nn-pn8j
-    # https://data.cityofnewyork.us/resource/43nn-pn8j.xml
-    table = doc.at('card.card.ng-isolate-scope')
-    table.search('tr').each do |tr|
-    cells = tr.search('th, td')
+    doc1 = Nokogiri::HTML(open("https://data.cityofnewyork.us/Health/DOHMH-New-York-City-Restaurant-Inspection-Results/43nn-pn8j"))
+    doc2 = Nokogiri::XML(open("https://data.cityofnewyork.us/resource/43nn-pn8j.xml"))
+    #name = doc.search('section.landing-page-section.dataset-preview').text
+    #ttps://data.cityofnewyork.us/Health/DOHMH-New-York-City-Restaurant-Inspection-Results/rs6k-p7g6    -results blank
+
+    # table = doc.at('card.card.ng-isolate-scope')
+    # table.search('tr').each do |tr|
+    # cells = tr.search('th, td')
+    #
     # cells.each do |cell|
     # text = cell.text.strip
     # puts CSV.generate_line(cells)
@@ -38,16 +41,7 @@ class NYCRestaurantInspection::Restaurant
       binding.pry
     end
   end
-end
-
-
-
-
-
-
-
-
-
+#end
 
 
 
