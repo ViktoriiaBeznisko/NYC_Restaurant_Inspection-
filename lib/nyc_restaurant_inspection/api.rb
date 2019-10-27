d0462ae03933a0f4760fe02b366b3be628a8275d
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-27 21:21:22 +0000

    11

--- lib/nyc_restaurant_inspection/api.rb
+++ lib/nyc_restaurant_inspection/api.rb
@@ -1,9 +1,37 @@
 class NYCRestaurantInspection::API
 
-  def gettind_date
+@@names = []
+@@boro = []
+@@zipcodes = []
+@@
+
+  def gettind_data
     url = "https://data.cityofnewyork.us/resource/43nn-pn8j.json"
     link = HTTParty.get(url)
     binding.pry
   end
 
+  def get_rest_name
+    self.gettind_data.each do |name|
+      p "Dba: #{name['dba']}"
+  end
+
+  def get_rest_address
+    self.gettind_data.each do |address|
+      p "Boro: #{address['boro']} | Zipcode: #{address['zipcode']}"
+  end
+
+  def get_rest_address
+    self.gettind_data.each do |inf|
+      p "Dba: #{inf['dba']} | Boro: #{inf['boro']} | Zipcode: #{inf['zipcode']} | Violation_desc: #{inf['violation_description']}"
+  end
+
 end
+
+
+
+# def gettind_date
+#   url = "https://data.cityofnewyork.us/resource/43nn-pn8j.json"
+#   link = HTTParty.get(url)
+#   binding.pry
+# end
