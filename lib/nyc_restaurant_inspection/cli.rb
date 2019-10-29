9d89a5a065ca74b243dc4cb2e85bf991a45e3ca8
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-29 21:46:48 +0000

    almost done

--- lib/nyc_restaurant_inspection/cli.rb
+++ lib/nyc_restaurant_inspection/cli.rb
@@ -1,5 +1,6 @@
 class NYCRestaurantInspection::CLI
 
+
   def call
     welcome
     list_restaurants
@@ -12,19 +13,19 @@ class NYCRestaurantInspection::CLI
     puts "Do you really want to know Health Inspection result for your favorite restaurant?[y/n]"
       input = gets.strip.downcase
 
-      if gets.strip == "y"
+      if input == "y"
         puts <<-DOC
         Please select from the list or type exit[1/2]:
         1 Search by name
         2 Search by zip code
         DOC
-      input = gets.strip.downcase
 
-      if gets.strip == "1"   #I have two times get strip ... should I create new methode ?....
+        input = gets.strip.downcase
+      if input == "1"   #I have two times get strip ... should I create new methode ?....
         search_by_name
-      elsif gets.strip == "2"
+      elsif input == "2"
         search_by_zip_code
-      elsif gets.strip == "exit"
+      elsif input == "exit"
         leave_page
       else
         puts "Not sure what you want..."
@@ -32,32 +33,34 @@ class NYCRestaurantInspection::CLI
     end
   end
 
-  def list_restaurants
-    @restaurants = NYCRestaurantInspection::Restaurant.all
-  end
+  # def list_restaurants
+  #   @restaurants = NYCRestaurantInspection::Restaurant.all
+  # end
 
-  def search_by_name
+  def search_by_name   #(name)
     input = nil
     while input != "exit"
       puts "Please type name of the restaurant:"
       input = gets.strip.downcase
-        if input.to_i > 0 # how to say if input =  name from array
-          result = @restaurants[input.to_i-1]
-        puts "#{result.name} - #{result.zip_code} - #{result.critical_flag} - #{result.violation_description}"
+      results =  NYCRestaurantInspection::Restaurant.rest_by_name(input)
+      list_results
       end
     end
-  end
 
   def search_by_zip_code
     input = nil
     while input != "exit"
       puts "Please type zip code:"
-      input = gets.strip.downcase # how to connect.. if input ...
-        if input == (number) #?????????
-        NYCRestaurantInspection::Restaurant.rest_by_zip_code(number) # how to connect with other results ? shold i initialize ?
+      input = gets.strip.downcase
+        NYCRestaurantInspection::Restaurant.rest_by_zip_code(input) # how to connect with other results ? shold i initialize ?
       end
     end
-  end
+
+
+    def list_results(restaurants)
+      restaurants.each{|restaurants| puts restaurants.name}  ###
+
+    end
 
   def leave_page
     puts "Eat, drink, be healthy! Goodbye!"
--- lib/nyc_restaurant_inspection/restaurant.rb
+++ lib/nyc_restaurant_inspection/restaurant.rb
@@ -16,21 +16,23 @@ class NYCRestaurantInspection::Restaurant
     @@all
   end
 
-  def self.uniq_zip_code
-    self.all.collect{|zip| zip.zip_code}.flatten.uniq
+  # def self.uniq_zip_code
+  #   self.all.collect{|zip| zip.zip_code}.flatten.uniq
+  # end
+
+  def self.rest_by_zip_code(zip_code)
+    # zip_code = self.get_zip_code[num-1]
+    # restaurants = self.all.collect{|rest|rest.name.include?(zip_code)}
+    # restaurants
+    self.all.select{|rest| rest.zip_code == zip_code}
+    
   end
 
-  def self.rest_by_zip_code(num)
-    zip_code = self.get_zip_code[num-1]
-    restaurants = self.all.collect{|rest|rest.name.include?(zip_code)}
-    restaurants
-  end
-
-    # def self.get_rest_name
-    #   self.all.collect{|rest_n| rest_n.name}
-    # end
+  # def self.get_rest_name
+  #     self.all.collect{|rest_n| rest_n.name}
+  #   end
     #
-    # def self.rest_by_name(name)
-    #   self.all.collect{|rest_n| rest_n.include?(name)}
-    # end
+  def self.rest_by_name(name)
+    self.all.select{|rest_n| rest_n.name.include?(name)}
+  end
 end
