da28dcf941d2b8b787d3d4bbedb02ddfd2252de2
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-11-01 14:45:16 +0000

    new logic

--- lib/nyc_restaurant_inspection/cli.rb
+++ lib/nyc_restaurant_inspection/cli.rb
@@ -3,57 +3,62 @@ class NYCRestaurantInspection::CLI
   def call
     NYCRestaurantInspection::API.getting_data
     welcome
-    #list_results
-    # search_by_name
-    # search_by_zip_code
-    leave_page
   end
 
   def welcome
     puts "Welcome to New York City Restaurant Inspection tracker!"
     puts "Do you really want to know Health Inspection result for your favorite restaurant?[y/n]"
-      input = gets.strip.downcase
-      if input == "y"
+    input = gets.strip.downcase
+    if input == "y"
+      menu
+    elsif input == "n" || input == "exit"
+      leave_page
+    else
+      not_found
+    end
+  end
 
-       puts <<-DOC.gsub /^\s*/, ''
-          Please select from the list or type exit[1/2]:
-          1 Search by name
-          2 Search by zip code
-        DOC
-        input = gets.strip.downcase
-      if input == "1"
-        search_by_name
-      elsif input == "2"
-        search_by_zip_code
-      elsif input == "exit"
-        leave_page
-      else
-        puts "Not sure what you want..."
-      end
+  def menu
+    puts <<-DOC.gsub /^\s*/, ''
+      Please select from the list or type exit[1/2]:
+      1 Search by name
+      2 Search by zip code
+    DOC
+    input = gets.strip.downcase
+    if input == "1"
+      search_by_name
+    elsif input == "2"
+      search_by_zip_code
+    elsif input == "exit"
+      leave_page
+    else
+      not_found
     end
   end
 
   def search_by_name
-      puts "Please type name of the restaurant:"
-      input = gets.strip.downcase
-      results =  NYCRestaurantInspection::Restaurant.rest_by_name(input)
-      #binding.pry
-      list_results(results)
-    end
-#  end
+    puts "Please type name of the restaurant:"
+    input = gets.strip.downcase
+    results =  NYCRestaurantInspection::Restaurant.rest_by_name(input)
+    list_results(results)
+    menu
+  end
 
   def search_by_zip_code
-    # input = nil
-    # while input != "exit"
-      puts "Please type zip code:"
-      input = gets.strip.downcase
-      results = NYCRestaurantInspection::Restaurant.rest_by_zip_code(input)
-      list_results(results)
-   end
-#  end
+    puts "Please type zip code:"
+    input = gets.strip.downcase
+    results = NYCRestaurantInspection::Restaurant.rest_by_zip_code(input)
+    list_results(results)
+    menu
+  end
 
   def list_results(restaurant)
-    restaurant.each{|rest| puts "- #{rest.name}","- #{rest.zipcode}","- #{rest.violation_description}","- #{rest.grade}"}
+    restaurant.each{|rest| puts "- Restaurant name: #{rest.name}","- Zip Code: #{rest.zipcode}","- Inspectors Report: #{rest.violation_description}","- Grade: #{rest.grade}", ""}
+  end
+
+  def not_found
+    puts "The search has not given any results"
+    menu
   end
 
   def leave_page
